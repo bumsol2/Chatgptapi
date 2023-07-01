3c1c416614ffc6bfb1d8fd5962bf5098854e256d
Sol <bumsol2@gmail.com>
2023-07-01 17:28:58 +0900

    0701

---
+++ chapter_python_01.py
@@ -0,0 +1,24 @@
+# 자료형과 변수
+# int, float, str, list tuple, set, dict
+# comment, 주석
+
+"""
+#int
+print(1+2+3)
+
+# float
+print(1.1)
+print(3.14)
+
+#str
+print("문자를 뜻하는 str 자료형")
+
+print("문자를 뜻하는 것이 str 자료형" + "같은 자료형끼리만 가능, 다른 자료형끼리는 연산 안됨")
+"""
+print("-" * 30 )
+print("안녕" )
+
+#변수
+a = 1
+b = 2
+print(a+b)
