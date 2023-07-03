d7572765aea81fc919bd3da2f213f77eff2a35cb
Sol <bumsol2@gmail.com>
2023-07-03 19:42:21 +0900

    0703

---
+++ chapter_python04.py
@@ -0,0 +1,27 @@
+"""
+모듈과 패키지
+모듈 :하나의 파이썬 파일, 이 파일에 들어있는 다른 함수나, 클래스, 변수들을 가져와서 사용 하 수가 있습니다.
+패키지: 모듈들을 묶어 놓은 폴더 
+"""
+# import overwatch as ow
+
+# print(ow.get_hp_by_character("genji"))
+
+# print(ow.hp_by_character)
+
+# from game.user import User
+# import game.user as u
+
+# user = u.User(username="python", rank="gold")
+# user.print_info()
+
+"""
+예외 처리 
+"""
+
+# try:
+#     print(1/0)
+# except Exception as e:
+#     print(f"error occur!, {e}")
+
+# print("here here")
